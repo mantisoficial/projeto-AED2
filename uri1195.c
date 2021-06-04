4de018bc4c7dec6964ffdf8105fe1852b650e3c2
Arthur <54213043+mantisoficial@users.noreply.github.com>
2021-06-04 20:54:10 -0300

    Create uri1195.c

---
+++ uri1195.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+struct node
+{
+    int data;
+    struct node *esq;
+    struct node *dir;
+};
+typedef struct node Node;
+
+int tipo;
+
+Node *AchaRaiz(Node *r, int v)
+{
+    if (r == NULL)
+        /* Node vazia */
+        return NULL;
+    else if (v <= r->data)
+        /* Novo noh deve ficar a esquerda de r */
+        if (r->esq == NULL)
+            return r;
+        else
+            return AchaRaiz(r->esq, v);
+    else
+        /* Novo noh deve ficar a direita de r */
+        if (r->dir == NULL)
+        return r;
+    else
+        return AchaRaiz(r->dir, v);
+}
+
+int ImprimeNode(Node *r)
+{
+    {
+        if (tipo == 1)
+        {
+            printf(" %d", r->data);
+            ImprimeNode(r->esq);
+            ImprimeNode(r->dir);
+        }
+        if (tipo == 2)
+        {
+            ImprimeNode(r->esq);
+            printf(" %d", r->data);
+            ImprimeNode(r->dir);
+        }
+        if (tipo == 3)
+        {
+            ImprimeNode(r->esq);
+            ImprimeNode(r->dir);
+            printf(" %d", r->data);
+        }
+    }
+    return 0;
+}
+int main()
+{
+    Node *raiz, *pai, *aux;
+    int tam, n_casos, c, d, noh;
+
+    raiz = NULL;
+    scanf("%d", &n_casos);
+
+    for (c = 1; c <= n_casos; c++)
+    {
+        raiz = NULL;
+
+        scanf("%d", &tam);
+        for (d = 0; d < tam; d++)
+        {
+            scanf("%d", &noh);
+
+            aux = (Node *)malloc(sizeof(Node));
+            aux->data = noh;
+            aux->esq = NULL;
+            aux->dir = NULL;
+
+            pai = AchaRaiz(raiz, noh);
+            if (pai == NULL)
+                raiz = aux;
+            else if (noh <= pai->data)
+                pai->esq = aux;
+            else
+                pai->dir = aux;
+        }
+
+        printf("Case %d:", c);
+
+        printf("\nPre.:");
+        tipo = 1;
+        ImprimeNode(raiz);
+
+        printf("\nIn..:");
+        tipo = 2;
+        ImprimeNode(raiz);
+
+        printf("\nPost:");
+        tipo = 3;
+        ImprimeNode(raiz);
+
+        printf("\n\n");
+    }
+
+    return 0;
+}
